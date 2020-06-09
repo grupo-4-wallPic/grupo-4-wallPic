494c4565fc9f554dc2d49d9b866c49aaa0ad8956
renzoIrusta <renzoirusta22@gmail.com>
2020-06-09 12:18:40 -0300

    method override

--- Sprint-2/app.js
+++ Sprint-2/app.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const path = require('path');
+const methodOverride = require('method-override')
 
 const homeRoutes = require('./routes/homeRoutes')
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));  
+app.use( methodOverride('_method'))
 
 app.listen(3000, () => console.log('running on 3000'));
 
