86fd9cd28f97fab34500bea18bf31be49df3522a
catagb <catagelabertb@Catalinas-MacBook-Pro.local>
2020-06-14 12:54:21 -0300

    category

--- Sprint-2/app.js
+++ Sprint-2/app.js
@@ -2,7 +2,9 @@
 const express = require('express');
 const path = require('path');
 
-const homeRoutes = require('./routes/homeRoutes')
+const homeRoutes = require('./routes/homeRoutes');
+const categoryRoutes = require ('./routes/categoryRoutes');
+//const productRoutes = require ('./routes/productRoutes')
 
 const app = express();
 
@@ -12,6 +14,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.listen(3000, () => console.log('running on 3000'));
 
 app.use('/', homeRoutes);
+app.use('/products/category', categoryRoutes);
+//app.use('/products/detail/:id', productRoutes);
 
 
 // module.exports = app;
---
+++ Sprint-2/controllers/categoryController.js
@@ -0,0 +1,16 @@
+const fs = require ('fs');
+
+const leerJson = fs.readFileSync('productsWallpicDataBase.json', {encoding: 'utf-8'})
+const products = JSON.parse (leerJson);
+
+module.exports = {
+
+    index: (req, res) => {
+        let animales = products.filter (function (product) {
+            return product.category == 'Animales'
+        })
+
+        return res.render('categories', {animales})
+    }
+    
+}
