10012a47bbc7e3bd7b718424e552a2f7cd2a2f25
carossino <manuelcarossino@me.com>
2020-06-16 04:39:02 -0300

    agregando primer parte home. pendiente medias querys y display de categorias

--- Sprint-2/controllers/homeController.js
+++ Sprint-2/controllers/homeController.js
@@ -1,7 +1,18 @@
-module.exports = {
+const fs = require ('fs');
+const path = require ('path');
+
+const homecontroller = {
 
     main: (req, res) => {
-        return res.render('index')
-    }
+
+    const productsFilePath = path.join(__dirname, '../productsWallpicDataBase.json');
+    const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+    let mostsale = products.filter(function(product){
+        return product.category == 'Mostsale'
     
-}
+    });
+        return res.render('index', {mostsale})
+    },
+};
+    module.exports = homecontroller;
