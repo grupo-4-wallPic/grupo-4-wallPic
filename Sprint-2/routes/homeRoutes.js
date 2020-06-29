fe72d5c5a654a5fd8fe000365d9c4a3890032095
renzoIrusta <renzoirusta22@gmail.com>
2020-06-29 12:42:02 -0300

    validator

--- Sprint-2/routes/homeRoutes.js
+++ Sprint-2/routes/homeRoutes.js
@@ -1,10 +1,12 @@
 const express = require ('express');
 const router = express.Router ();
 const homeController = require('../controllers/homeController')
+const loginController = require('../controllers/loginController')
+const validator = require('../middlewares/validator')
 
-
-router.get('/', homeController.main)
-
+router.get('/', homeController.main);
+router.post('/login', loginController.login);
+router.post('/register',validator.registerValidator, loginController.register);
 
 
 module.exports = router;
