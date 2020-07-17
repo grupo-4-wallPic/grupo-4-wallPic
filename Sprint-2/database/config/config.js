4bc4e1038174510bf3111cd4453a8fe6e771cfb6
renzoIrusta <renzoirusta22@gmail.com>
2020-07-17 20:36:36 -0300

    ultimos cambios

--- Sprint-2/database/config/config.js
+++ Sprint-2/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": 'cosmonauta2020',
+    "password": null,
     "database": "wallpic",
     "host": "127.0.0.1",
     "dialect": "mysql"
